687747a7eed64c7ba2fa1d1d986a873c0afcd7f7
SurbhiAbrol <surbhiabrol24@gmail.com>
2019-01-17 15:35:04 +0530

    done

---
+++ src/main/java/com/stackroute/MuzixApplication/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.stackroute.MuzixApplication.repository;
+
+import com.stackroute.MuzixApplication.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
